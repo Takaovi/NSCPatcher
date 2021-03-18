e889d66a16852927b1c4365409fb964e82dd43c5
Takaovi <50058811+Takaovi@users.noreply.github.com>
2021-03-18 02:32:19 +0200

    Revert "Supports now more APKs"
    This reverts commit afe5df83e50d65a0b10b50c2f0fec9eea1f1c60d.

---
+++ NSCPatcher/NSCPatcher_Data/compile.bat
@@ -0,0 +1,3 @@
+cd NSCPatcher_Data\<name>
+java -jar apktool.jar b -f -o <path2>
+exit
--- NSCPatcher/Window.cs
+++ NSCPatcher/Window.cs
@@ -13,7 +13,6 @@ namespace NSCPatcher
     {
         string APKname = "";
         int seconds = 0;
-        bool debugmode = false;
 
         public Window()
         {
@@ -43,32 +42,29 @@ namespace NSCPatcher
                 timer.Start();
 
                 PatchButton.Enabled = false;
-                debugmodecheckbox.Enabled = false;
                 progressBar1.Value = 0;
                 Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Starting to patch your APK..."; }));
 
                 APKname = Path.GetFileName(fileD.FileName).Replace(" ", "_");
                 File.Copy(fileD.FileName, AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKname, true);
 
-                resetbat();
                 decompile();
             }
         }
 
         private void decompile()
         {
-            string str = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat");
+            string str = File.ReadAllText((AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat"));
             str = str.Replace("<path>", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKname);
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat", str);
 
-            string str6 = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat");
+            string str6 = File.ReadAllText((AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat"));
             str6 = str6.Replace("apktool.jar", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\apktool.jar");
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat", str6);
 
             Process p = new Process();
             p.StartInfo.FileName = "cmd.exe";
-            if (!debugmode)
-                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             p.StartInfo.Arguments = "/k \"" + AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\" + "decompile.bat" + "\"";
             p.EnableRaisingEvents = true;
             p.Exited += new EventHandler(p_Exited);
@@ -94,6 +90,8 @@ namespace NSCPatcher
 
             string biggestfoldername = "xml";
 
+            int count = 0;
+
             //Get the file name without ".apk"
             string APKfolder = APKname.Remove(APKname.Length - 4);
 
@@ -102,37 +100,16 @@ namespace NSCPatcher
 
             Program.takaovi_github.progressBar1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.progressBar1.Value = 50; }));
             Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Placing the network security config..."; }));
-
-            // Copy network_security_config file to the application /////////
+            //Copy network_security_config file to the application's xml path//////////
             try
             {
                 if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml"))
-                    if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml"))
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\network_security_config.xml", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml");
-                    else
-                    {
-                        string str = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml");
-                        if (str.Contains("<trust-anchors>"))
-                        {
-                            if (!str.Contains("<certificates src=\"user\" />"))
-                                str = str.Replace("<trust-anchors>", "<trust-anchors>\n" + "            <certificates src=\"user\" />");
-                            if (!str.Contains("<certificates src=\"system\" />"))
-                                str = str.Replace("<trust-anchors>", "<trust-anchors>\n" + "            <certificates src=\"system\" />");
-                            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml", str);
-                        }
-                        else
-                        {
-                            File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml");
-                            File.Copy(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\network_security_config.xml", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml");
-                        }
-                    }
+                    File.Copy(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\network_security_config.xml", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\xml\network_security_config.xml");
 
                 else
                 {
-                    //Find the folder with the most xml
-                    /*
-                    int count = 0;
                     var allFolders = new List<string>();
+                    string path = AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\";
                     foreach (string folder in Directory.GetDirectories(path))
                     {
                         if (count < (from file in Directory.EnumerateFiles(folder, "*.xml", SearchOption.AllDirectories) select file).Count())
@@ -142,16 +119,12 @@ namespace NSCPatcher
                             biggestfoldername = name;
                         }
                     }
-                    */
-
-                    string path = AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\";
-
-                    Directory.CreateDirectory(path + @"xml");
-                    biggestfoldername = "xml";
 
                     if (Directory.Exists(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\" + biggestfoldername))
                         File.Copy(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\network_security_config.xml", AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\" + biggestfoldername + @"\network_security_config.xml");
                     ManifestPatchString = "android:networkSecurityConfig=\"@" + biggestfoldername + "/network_security_config\"";
+
+                    //MessageBox.Show(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\res\" + biggestfoldername + @"\network_security_config.xml");
                 }
             }
             catch
@@ -160,8 +133,7 @@ namespace NSCPatcher
 
             Program.takaovi_github.progressBar1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.progressBar1.Value = 55; }));
             Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Editing the manifest..."; }));
-
-            //// Edit the Manifest file //////////////////////////////////////////////
+            ////Edit the Manifest file//////////////////////////////////////////////
             try
             {
                 string str3 = File.ReadAllText((AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\Temp\" + APKfolder + @"\AndroidManifest.xml"));
@@ -199,10 +171,9 @@ namespace NSCPatcher
                 timer.Stop();
                 seconds = 0;
                 Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Failed"; }));
-                MessageBox.Show("Something went wrong with the patch.\nThe AndroidManifest.xml was probably corrupted and it couldn't be edited.", "Oops!");
+                MessageBox.Show("Something went wrong with the patch.\nYou most likely need to patch the application manually as the APK is probably using a different location for network_security_config.xml.", "Oops!");
                 resetbat();
                 Program.takaovi_github.PatchButton.Invoke(new MethodInvoker(delegate { Program.takaovi_github.PatchButton.Enabled = true; }));
-                Program.takaovi_github.debugmodecheckbox.Invoke(new MethodInvoker(delegate { Program.takaovi_github.debugmodecheckbox.Enabled = true; }));
                 Program.takaovi_github.progressBar1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.progressBar1.Value = 0; }));
                 Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Ready to patch..."; }));
             }
@@ -232,8 +203,7 @@ namespace NSCPatcher
             //Start compiling APK
             Process p = new Process();
             p.StartInfo.FileName = "cmd.exe";
-            if (!debugmode)
-                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             p.StartInfo.Arguments = "/k \"" + AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\" + "compile.bat" + "\"";
             p.EnableRaisingEvents = true;
             p.Exited += new EventHandler(p_Exited);
@@ -257,8 +227,7 @@ namespace NSCPatcher
             //Start signing APK
             Process p = new Process();
             p.StartInfo.FileName = "cmd.exe";
-            if (!debugmode)
-                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             p.StartInfo.Arguments = "/k \"" + AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\" + "sign.bat" + "\"";
             p.EnableRaisingEvents = true;
             p.Exited += new EventHandler(p_Exited);
@@ -269,6 +238,8 @@ namespace NSCPatcher
                 //If compiled successfully
                 if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"Patched_APKs\" + APKname))
                 {
+                    //File.Delete(AppDomain.CurrentDomain.BaseDirectory + @"Patched_APKs\" + APKname);
+                    //File.Move(AppDomain.CurrentDomain.BaseDirectory + @"Patched_APKs\" + APKname + "_", AppDomain.CurrentDomain.BaseDirectory + @"Patched_APKs\" + APKname + "_".Replace("_", ""));
 
                     resetbat();
 
@@ -284,7 +255,6 @@ namespace NSCPatcher
                     Process.Start(AppDomain.CurrentDomain.BaseDirectory + @"Patched_APKs");
 
                     Program.takaovi_github.PatchButton.Invoke(new MethodInvoker(delegate { Program.takaovi_github.PatchButton.Enabled = true; }));
-                    Program.takaovi_github.debugmodecheckbox.Invoke(new MethodInvoker(delegate { Program.takaovi_github.debugmodecheckbox.Enabled = true; }));
                 }
                 else
                 {
@@ -293,7 +263,6 @@ namespace NSCPatcher
                     Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Failed"; }));
                     MessageBox.Show("Something went wrong with compiling...", "Oops!");
                     resetbat();
-                    Program.takaovi_github.debugmodecheckbox.Invoke(new MethodInvoker(delegate { Program.takaovi_github.debugmodecheckbox.Enabled = true; }));
                     Program.takaovi_github.PatchButton.Invoke(new MethodInvoker(delegate { Program.takaovi_github.PatchButton.Enabled = true; }));
                     Program.takaovi_github.progressBar1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.progressBar1.Value = 0; }));
                     Program.takaovi_github.groupBox1.Invoke(new MethodInvoker(delegate { Program.takaovi_github.groupBox1.Text = "Ready to patch..."; }));
@@ -351,21 +320,16 @@ namespace NSCPatcher
             resetbat();
         }
 
-        string end = "";
         private void resetbat()
         {
-            if (debugmode)
-                end = "pause";
-            else end = "exit";
-
             //Reverse edits on compile.bat
-            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\compile.bat", @"cd NSCPatcher_Data\Temp\<name>" + "\njava -jar apktool.jar --use-aapt2 b -f -o \"<path2>\"" + "\n" + end);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\compile.bat", @"cd NSCPatcher_Data\Temp\<name>" + "\njava -jar apktool.jar b -f -o <path2>" + "\nexit");
 
             //Reverse edits on decompile.bat
-            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat", @"cd NSCPatcher_Data\Temp" + "\njava -jar \"apktool.jar\" --use-aapt2 d -f -k -s --no-assets --only-main-classes \"<path>\"" + "\n" + end);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\decompile.bat", @"cd NSCPatcher_Data\Temp" + "\njava -jar apktool.jar d -f <path>" + "\nexit");
 
             //Reverse edits on sign.bat
-            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\sign.bat", "cd NSCPatcher_Data" + "\njava -jar \"apksigner.jar\" sign --key \"key.pk8\" --cert \"cert.pem\" --v4-signing-enabled false --out \"<apkpath>\" \"<apkpath>\"" + "\n" + end);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"NSCPatcher_Data\sign.bat", "cd NSCPatcher_Data" + "\njava -jar \"apksigner.jar\" sign --key \"key.pk8\" --cert \"cert.pem\" --v4-signing-enabled false --out \"<apkpath>\" \"<apkpath>\"" + "\nexit");
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -377,13 +341,6 @@ namespace NSCPatcher
         private void Window_FormClosed(object sender, FormClosedEventArgs e)
         {
             //Nothing
-        }
-
-        private void debugmodecheckbox_CheckedChanged(object sender, EventArgs e)
-        {
-            if (debugmode)
-                debugmode = false;
-            else debugmode = true;
         }
     }
 }
